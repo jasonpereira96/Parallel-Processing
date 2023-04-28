12359bfd1118ffc3d5ff4179c71459bba5f8c898
Jason Pereira <jasonpereira074@gmail,com>
2023-04-27 21:17:14 -0700

    adding pbs.sh

---
+++ pbs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -N pi_jason
+#PBS -l walltime=00:00:15
+#PBS -l nodes=4:ppn=1
+#PBS -q edu_shared
+#PBS -j oe
+#
+## Move to the directory where the job was submitted
+#
+cd $PBS_O_WORKDIR
+#
+#  setup MPI programming environment
+#
+module load MPICH/3.3.2-GCC-9.3.0
+#
+# Build mpi job
+#
+mpic++ qs.cpp
+#
+# Run mpi job
+#
+# mpirun -np $PBS_NP ./pi_jason
+mpirun ./a.out 16
