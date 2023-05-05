4fa1abf5001fcc2c99a19382f3e5de96ea1c6992
Meer Shah <mshah229@login-2.extreme.acer.uic.edu>
2023-05-04 21:51:05 -0500

    Changed run.sh again

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm qs.out
 mpic++ qs.cpp -o qs.out
-mpiexec -n 16 ./qs.out test/A_vector_10000_16_4.txt tes/pivot_vector_10000_16_4.txt 0 4
+mpiexec -n 16 ./qs.out test/A_vector_100000_16_4.txt test/pivot_vector_100000_16_4.txt 0 4
 
  
